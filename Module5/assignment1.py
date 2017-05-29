191f56904b1fefebd441e3fde63f52b6936959d8
Siddhartha <siddharthabose@live.com>
2017-05-29 20:49:12 +0530

    K-Means clustering

--- Module5/assignment1.py
+++ Module5/assignment1.py
@@ -1,12 +1,15 @@
 #
 # TODO: Import whatever needs to be imported to make this work
 #
-# .. your code here ..
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+from sklearn.cluster import KMeans
 
 
 # Look Pretty
 # matplotlib.style.use('ggplot')
-#plt.style.use('ggplot')
+plt.style.use('ggplot')
 
 
 #
@@ -18,32 +21,31 @@
 # 3. Scroll down to 'GAMBLING'
 # 4. Click the light blue 'Export' button next to the 'Filter' button, and select 'Download As CSV'
 
-
-
 def doKMeans(df):
   #
   # INFO: Plot your data with a '.' marker, with 0.3 alpha at the Longitude,
   # and Latitude locations in your dataset. Longitude = x, Latitude = y
   fig = plt.figure()
   ax = fig.add_subplot(111)
-  ax.scatter(df.Longitude, df.Latitude, marker='.', alpha=0.3)
+  ax.scatter(df.Longitude, df.Latitude, marker='.', alpha=0.3, c='green')
 
   #
   # TODO: Filter df so that you're only looking at Longitude and Latitude,
   # since the remaining columns aren't really applicable for this purpose.
   #
-  # .. your code here ..
+  k_means_data = df[['Longitude', 'Latitude']]
 
   #
   # TODO: Use K-Means to try and find seven cluster centers in this df.
   # Be sure to name your kmeans model `model` so that the printing works.
   #
-  # .. your code here ..
+  kmeans_model = KMeans(n_clusters=7)
+  kmeans_model.fit(k_means_data)
 
   #
   # INFO: Print and plot the centroids...
-  centroids = model.cluster_centers_
-  ax.scatter(centroids[:,0], centroids[:,1], marker='x', c='red', alpha=0.5, linewidths=3, s=169)
+  centroids = kmeans_model.cluster_centers_
+  ax.scatter(centroids[:,0], centroids[:,1], marker='^', c='red', alpha=0.9, linewidths=3, s=169)
   print centroids
 
 
@@ -51,27 +53,26 @@ def doKMeans(df):
 #
 # TODO: Load your dataset after importing Pandas
 #
-# .. your code here ..
-
+df = pd.read_csv('Datasets/Crimes.csv')
 
 #
 # TODO: Drop any ROWs with nans in them
 #
-# .. your code here ..
-
+df.dropna(axis=0, how='any', inplace=True)
+df.reset_index(drop=True)
 
 #
 # TODO: Print out the dtypes of your dset
 #
-# .. your code here ..
+print df.dtypes
 
 
 #
 # Coerce the 'Date' feature (which is currently a string object) into real date,
 # and confirm by re-printing the dtypes. NOTE: This is a slow process...
 #
-# .. your code here ..
-
+df['Date'] = pd.to_datetime(df['Date'])
+print df.dtypes
 
 # INFO: Print & Plot your data
 doKMeans(df)
@@ -82,7 +83,7 @@ doKMeans(df)
 # a Date > '2011-01-01', using indexing. Then, in a new figure, plot the
 # crime incidents, as well as a new K-Means run's centroids.
 #
-# .. your code here ..
+df = df[df['Date'] > '2011-01-01']
 
 
 
